1df0ac139e83493c5d0cd22e531e02fa9f70e19b
fazmirfawasps <fazmirfawasps@gmail.com>
2023-07-02 03:34:48 +0530

    Your third  message here

--- adminside/src/axios/axios.js
+++ adminside/src/axios/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:7000',
+  baseURL: 'http://htron.site/api',
   headers: {
     'Content-Type': 'application/json',
   },
