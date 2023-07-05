1e19fca94cf81aa787c79a129aeb9947a9b33185
fazmirfawasps <fazmirfawasps@gmail.com>
2023-07-05 13:15:40 +0530

    update nouse route

--- homeside/src/components/navbar.jsx
+++ homeside/src/components/navbar.jsx
@@ -12,7 +12,7 @@ import Button from '@mui/material/Button';
 import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 // import AdbIcon from '@mui/icons-material/Adb';
-import api from '../axios/axios';
+// import api from '../axios/axios';
 import { useEffect } from 'react';
 // import ModalDialog from './modal';
 import { auth, } from '../firebase/firebase';
